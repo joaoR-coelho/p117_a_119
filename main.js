d4ec21bcaf383a044bb94728407aa4330e8ab08f
joaoR-coelho <111904638+joaoR-coelho@users.noreply.github.com>
2023-02-27 15:44:29 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function setup() {
+    canvas=createCanvas(300,300)
+    canvas.center()
+    background("darkgreen")    
+}
+function preload() {
+
+}
+function clearCanvas() {
+   background("darkgreen")
+   
+}
+function draw() {
+   
+}
+function classifyCanvas() {
+   
+}
